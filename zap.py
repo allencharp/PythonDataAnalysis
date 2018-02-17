c92632996489be0868591cdfa0df0398402cbc2b
allen <liuzhidong_1985@hotmail.com>
2018-02-17 20:21:13 +0800

    continue

--- zap.py
+++ zap.py
@@ -1,7 +1,6 @@
 path = 'usagov.txt'
 
 result = open(path).readline()
-print(result)
 
 import json
 records = [json.loads(line) for line in open(path,encoding="utf8")]
@@ -10,3 +9,39 @@ print(records[0]['tz'])
 
 time_zone = [record['tz'] for record in records if 'tz' in record and record['tz'] != '']
 print(time_zone[:5])
+
+from collections import defaultdict
+
+def get_counts2(sequese):
+    counts = defaultdict(int)
+    for x in sequese:
+        counts[x] += 1
+    return counts
+
+counts = get_counts2(time_zone)
+
+print(counts['America/New_York'])
+
+print(len(time_zone))
+
+def top_counts(count_dict, n=5):
+    value_key_pairs = [(count, tz) for tz,count in count_dict.items()]
+    value_key_pairs.sort()
+    return value_key_pairs[-n:]
+
+print(top_counts(counts))
+
+from pandas import DataFrame, Series
+import pandas as pd
+import numpy
+frame = DataFrame(records)
+print(frame['tz'][:10])
+
+clean_tz = frame['tz'].fillna('Missing')
+print(clean_tz[:10])
+clean_tz[clean_tz == ''] = 'Unknown'
+tz_counts = clean_tz.value_counts()
+print(tz_counts[:10])
+plt = tz_counts[:10].plot(kind='barh',rot=0)
+
+a = 1
